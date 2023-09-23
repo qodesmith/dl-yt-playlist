65b72f1360aef21ebf1096f8f99207bc20cee52d
Qodesmith <theqodesmith@gmail.com>
2023-09-23 18:28:57 -0400

    Logging build time

--- build.ts
+++ build.ts
@@ -1,5 +1,7 @@
 import dts from 'bun-plugin-dts'
 
+const start = performance.now()
+
 await Bun.build({
   entrypoints: ['./src/main.ts'],
   outdir: './dist',
@@ -7,3 +9,6 @@ await Bun.build({
   external: ['@googleapis/youtube'],
   plugins: [dts()],
 })
+
+const totalTime = ((performance.now() - start) / 1000).toFixed(2)
+console.log(`Finished building in ${totalTime} seconds`)
